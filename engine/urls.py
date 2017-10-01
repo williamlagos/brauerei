e0b8202ebb77958fddb62d984410eb6b02267338
William Oliveira de Lagos <william.lagos@live.com>
2017-10-01 19:03:02 -0300

    Added configuration for Postgres connection

--- engine/urls.py
+++ engine/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url, include
 from engine.api import *
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name="empty.html")),
     url(r'^products$', ProductResource.as_list()),
     url(r'^products/(?P<pk>\d+)$', ProductResource.as_detail()),
     url(r'^stocks$', StockResource.as_list()),
