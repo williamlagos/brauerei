3061ef729041a078df7fb9891036da824cd6a95b
William Oliveira de Lagos <william.lagos@live.com>
2017-10-05 18:21:37 -0300

    Refreshed forms and breadcrumbs to bootstrap format

--- vupit/settings.py
+++ vupit/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = [ '*' ]
 # Application definition
 
 INSTALLED_APPS = [
-    'flat_responsive',
+    'widget_tweaks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
